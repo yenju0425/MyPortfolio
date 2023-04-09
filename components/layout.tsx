d23e444b42b8b6f9dd09d68b552d331059bfe2d9
yenju0425 <yenju0425@gmail.com>
2023-04-09 22:27:00 +0800

    complete Landing page.

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import Footer from './footer';
 import Header from './header';
+import styles from '@/styles/Layout.module.css'
 
 /* Type checking for props */
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <>
+    <main className={styles.main}>
       <Header />
-      <main>{children}</main>
+        <div>
+          {children}
+        </div>
       <Footer />
-    </>
+    </main>
   );
 }
 
