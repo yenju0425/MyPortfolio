df2f13ec341854e118f9f6d7ec055aed41834350
yenju0425 <yenju0425@gmail.com>
2023-07-13 01:58:59 +0800

    feat: update the layouts, and add comming soon to unfinished pages

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,7 +1,46 @@
-export default function About() {
+import Link from 'next/link';
+import styles from '@/styles/About.module.css'
+import { useEffect, useState } from 'react';
+
+export default function Contact() {
+  let commingSoonIndex = 0;
+  const comningSoons = ["Comming Soon", ".Comming Soon.", "..Comming Soon..", "...Comming Soon..."];
+
+  const [commingSoonString, setCommingSoonString] = useState(comningSoons[commingSoonIndex]);
+
+  const setCommingSoonTimer = () => {
+    setTimeout(() => {
+      console.log(commingSoonIndex);
+      commingSoonIndex = (commingSoonIndex + 1) % comningSoons.length;
+      setCommingSoonString(comningSoons[commingSoonIndex]);
+      setCommingSoonTimer();
+    }, 1000);
+  }
+
+  useEffect(() => {
+    console.log("useEffect");
+    setCommingSoonTimer();
+  }, []);
+
   return (
     <>
-      <p> This is the Contact Page </p>
+      <main className={styles.main}>
+        <div>
+        </div>
+
+        <div className={styles.contents}>
+          <h1 className={styles.title}>
+            {commingSoonString}
+          </h1>
+
+          <Link href="/Games/sng" className={styles.pokerLink}>
+            <span>Check out the SNG Poker Game</span>
+          </Link>
+        </div>
+
+        <div>
+        </div>
+      </main>
     </>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import Head from 'next/head'
 import Link from 'next/link';
-import Image from 'next/image'
 import styles from '@/styles/Home.module.css'
 
 export default function Home() {
